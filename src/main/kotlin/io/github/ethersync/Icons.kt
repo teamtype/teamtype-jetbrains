c38fa31d339e0a6159bc73be4793539b7ab0fb1d
Marc Schreiber <info@schrieveslaach.de>
2025-02-22 19:37:56 +0100

    Use New UI Icons
    Follow the Jetbrains guide to provide icons, see
    https://plugins.jetbrains.com/docs/intellij/icons.html#new-ui-icons
    
    Fixes #2

--- src/main/kotlin/io/github/ethersync/Icons.kt
+++
@@ -1,8 +0,0 @@
-package io.github.ethersync
-
-import com.intellij.openapi.util.IconLoader
-
-object Icons {
-   @JvmField
-   val PluginIcon = IconLoader.getIcon("/META-INF/pluginIcon.svg", javaClass)
-}
